67e7406edec9809366bf757b8bfe917453c64897
holmze <704062177@qq.com>
2024-01-03 21:06:12 +0800

    update HW2 Server

--- AP1400-2-HW2/include/server.h
+++ AP1400-2-HW2/include/server.h
@@ -1,5 +1,51 @@
 #ifndef SERVER_H
 #define SERVER_H
+#include<stdlib>
 
+#include<map>
+#include<iostream>
+#include<string>
 
+#include "client.h"
+#include "crypto.h"
+
+using std::cout;
+using std::endl;
+
+class Client;
+
+ std::vector<std::string> pending_trxs;
+
+class Server
+{
+public:
+	Server();
+	std::shared_ptr<Client> add_client(std::string id);
+	//This function will create a new Client with the specified id. If this id already exists, the server should add a random 4 digit number at the end of it automatically.
+	//::UPDATE:: each client should be assigned with 5 coins at the begining.
+	//note. do not use srand for your random numbers.
+
+	std::shared_ptr<Client> get_client(std::string id);
+	//Using this function you can get a pointer to a Client using its id.
+
+	double get_wallet(std::string id);
+	//Using this function will return the wallet value of the client with username id.
+
+	bool parse_trx(std::string trx, std::string sender, std::string receiver, double value);
+	//Each transaction has 3 properties: i) id of the sender ii) id of the receiver iii) value of money to transfer. We will show each transaction with a string, concatenating each of these properties with a -. For example if ali sends 1.5 coins to hamed the transaction will be shown by a string "ali-hamed-1.5". This function will parse this string format and outputting each property separately, if the string is not standard you should throw a runtime error.
+
+	bool add_pending_trx(std::string trx, std::string signature);
+	//Each Client can add a pending transaction using the transaction format described in the above section. Only accept a pending transaction by authenticating the sender's signature and if he has enough money in his wallet. note. define the below variable outside the Server class and save the pending transactions in it.
+
+	size_t mine();
+	/*
+	As mentioned in the TA class each transaction has a pending state until it has been mined and to mine transactions you first need to put your pending transactions together: For example if you have 3 transactions like "ali-hamed-1.5", "mhmd-maryam-2.25", and "mahi-navid-0.5"; You will get one final string as: "ali-hamed-1.5mhmd-maryam-2.25mahi-navid-0.5". We call this string the mempool. You will also add a number called nonce at the end of this string. To mine the transactions the server will generate the mempool and asks each Client for a nonce and calculates the sha256 of the final string. For each nonce if the generated sha256 has 4 zeros in a row in the first 10 numbers, then the mine is successful and the client who called the correct nonce will be awarded with 6.25 coins. after a successful mine of the pending transactions, all the transactions will be removed from pending and the effect of them will be applied on the clients.
+	::UPDATE:: instead of 4 zeros use 3 zeros in a row so it wont take time for your runs.
+	note. after a successful mine, print the id of the miner and return the associate nonce.
+	*/
+
+private:
+	std::map<std::shared_ptr<Client>, double> clients;//This member variable will map each client to its wallet. The wallet is the amount of money the client has. 
+};
+void show_wallets(const Server& server);
 #endif //SERVER_H
--- AP1400-2-HW2/src/server.cpp
+++ AP1400-2-HW2/src/server.cpp
@@ -1,1 +1,166 @@
 #include "server.h"
+#include <random>
+#include <sstream>
+using std::runtime_error;
+
+std::map<std::shared_ptr<Client>,double>> Server::get_clients() const{
+    return this->clients;
+}
+
+Server::Server(){
+    this->client = {};
+}
+
+std::string random4num(){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(0, 9);
+
+    std::string result = "";
+    for (int i = 0; i < 4; ++i) {
+        int num = dis(gen);
+        result += std::to_string(num);
+    }
+
+    return result;
+}
+
+std::shared_ptr<Client> add_client(std::string id){
+    bool isExist{false};
+    for(auto& [client, amount]: client){
+        if(client->get_id()==id){
+            isExist = true;
+            break;
+        }
+    }
+    
+    std::string newID = id;
+    if(isExist){
+        newID += random4num();
+    }
+
+    Client* newClient = new Client(newID,*this);
+    std::shared_ptr<Client> sp(newClient);
+    this->clients[sp] = 5;
+    return sp;
+}
+
+std::shared_ptr<Client> get_client(std::string id){
+    for(auto& [client, amount]: client){
+        if(client->get_id()==id){
+            return client;
+        }
+    }
+    return nullptr;
+}
+double get_wallet(std::string id){
+    // auto sp = get_client(id);
+    // if(sp == nullptr){
+    //     return 0;
+    // }
+    // return this->clients[sp];
+    for(auto& [client, amount]: client){
+        if(client->get_id()==id){
+            return amount;
+        }
+    }
+    return 0;
+}
+
+bool parse_trx(std::string trx, std::string sender, std::string receiver, double value){
+    std::istringstream iss(input);
+    std::string token;
+
+    // 解析第一个字符串
+    if (!std::getline(iss, token, '-'))
+        return false;
+    str1 = token;
+
+    // 解析第二个字符串
+    if (!std::getline(iss, token, '-'))
+        return false;
+    str2 = token;
+
+    // 解析 double 值
+    if (!(iss >> value)){
+        throw runtime_error("something bad");
+        return false;
+    }
+        
+    // 检查是否已解析整个输入字符串
+    if (iss.rdbuf()->in_avail() != 0){
+        throw runtime_error("something bad");
+        return false;
+    }
+        
+    return true;
+}
+
+bool add_pending_trx(std::string trx, std::string signature){
+    std::string sender{};
+    std::string receiver{};
+    double value = 0;
+    try{
+        parse_trx(trx, sender, receiver, value);
+    }
+    catch(runtime_error){
+        return false;
+    }
+    pending_trxs.push_back(trx);
+
+    if(get_wallet(sender)<value){
+        return false;
+    }
+    else{
+        this->clients[this->get_client(sender)] -= value;
+        this->clients[this->get_client(receiver)] += value;
+    }
+    return true;
+}
+
+bool hasConsecutiveZeros(const std::string& str) {
+    if (str.length() < 10) {
+        // 字符串长度不足10，不可能有连续的三个零
+        return false;
+    }
+
+    std::string prefix = str.substr(0, 10);
+    std::string target = "000";
+
+    return (prefix.find(target) != std::string::npos);
+}
+
+size_t mine(){
+    if(pending_trxs.size()==0){
+        return 0;
+    }
+    
+    std::string mempool;
+    for(int i = 0;i<pending_trxs.size();i++){
+        mempool+=pending_trxs[i];
+    }
+    
+
+    cout << mempool <<endl;
+
+    for(auto& [client, amount]:clients){
+        auto nonce = client->generate_nonce();
+        std::string mempoolNonce = mempool+nonce;
+        std::string hashValue{crypto::sha256(mempoolNonce)};
+        if(hasConsecutiveZeros(hashValue)){
+            amount += 6.5;
+            cout << get_id(client) <<endl;
+            pending_trxs.clear();
+            return nonce;
+        }
+    }
+    return 0;
+}
+
+void show_wallets(const Server& server) {
+    std::cout << std::string(20, '*') << std::endl;
+    for (const auto& client : server.get_clients())
+        std::cout << client.first->get_id() << " : " << client.second << std::endl;
+    std::cout << std::string(20, '*') << std::endl;
+}
+
