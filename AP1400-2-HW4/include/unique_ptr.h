ded5c3bf941e2f150ab108b757a7ffbd69b0be6d
holmze <704062177@qq.com>
2024-01-09 14:53:38 +0800

    update UniquePtr

--- AP1400-2-HW4/include/unique_ptr.h
+++ AP1400-2-HW4/include/unique_ptr.h
@@ -1,8 +1,57 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
+#include<iostream>
+#include<memory>
+using std::cout;
+using std::endl;
+// using std::make_unique;
+template <typename T>
+class UniquePtr {
+    public:
+        explicit UniquePtr(T* ptr = nullptr) : _p(ptr){}
+        explicit UniquePtr(std::unique_ptr<T> ptr) : _p(ptr.release()){}
+
+        // explicit UniquePtr() : _p(nullptr){}
+        ~UniquePtr(){delete _p;}
+        T* get(){
+            return _p;
+        }
+        T operator*() const {
+            return *_p;
+        }
+        T* operator->() const{
+            return _p;
+        }
+        void reset(){
+            delete _p;
+            _p = nullptr;
+        }
+        template<typename U>
+        void reset(U* ptr){
+            delete _p;
+            _p = ptr;
+        }
+        T* release(){
+            T* releasePtr = _p;
+            _p = nullptr;
+            return releasePtr;
+        }
+        explicit operator bool() const{
+            return _p != nullptr;
+        }
 
 
 
+    private:
+        T* _p;
+    // 类定义和成员函数声明/定义
+    // 可以使用类型参数 T 作为模板参数
+};
+
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args){
+    return std::make_unique<T>(std::forward<Args>(args)...);
+}
 
 
 
--- AP1400-2-HW4/src/main.cpp
+++ AP1400-2-HW4/src/main.cpp
@@ -5,10 +5,28 @@
 
 int main(int argc, char **argv)
 {
-    if (true) // make false to run unit-tests
+    if (false) // make false to run unit-tests
     {
-        // debug section 
-        
+        // debug section
+        // UniquePtr<int> ptr{new int{10}};
+        // // cout << *ptr <<endl;
+        // auto up = make_unique<int>(42);
+
+        // UniquePtr<int> ptr1;
+        // cout << ptr.get() <<endl;
+        // cout << *ptr <<endl;
+        // UniquePtr<std::string> sptr{new std::string{"hello"}};
+        // cout << sptr->length() <<endl;
+        // // UniquePtr<std::string> ptr(new std::string("hello"));
+        // sptr.reset(new std::string("nice"));
+        // std::cout << *sptr << std::endl;  // 输出：nice
+
+        // UniquePtr<double> dptr{new double{1.567}};
+        // double *tmp{dptr.release()};
+        // std::cout << *tmp << std::endl; // output: 1.567
+        // delete tmp; // manual deletion
+
+        // UniquePtr<int> ptr1{make_unique<int>(10)};
     }
     else
     {
