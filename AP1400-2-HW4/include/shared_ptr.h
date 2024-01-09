294972ae2d80c6ab159880960d0ecff7f7bdd3e0
holmze <704062177@qq.com>
2024-01-09 16:41:41 +0800

    finish HW4 with chatGPT, used 1 afternoon

--- AP1400-2-HW4/include/shared_ptr.h
+++ AP1400-2-HW4/include/shared_ptr.h
@@ -1,5 +1,105 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
+#include<iostream>
+#include<memory>
+#include <atomic>
+
+using std::cout;
+using std::endl;
+template <typename T>
+class SharedPtr {
+    public:
+        SharedPtr(T* ptr = nullptr) : _p(ptr), _refCount(new size_t(1)){
+            // cout << "this is SharedPtr(p)"<<endl;
+        }
+        template<typename U>
+        SharedPtr(const std::shared_ptr<U>& ptr) : _p(ptr.get()), _refCount(new size_t(ptr.use_count())){
+            if(_refCount){
+                ++(*_refCount);
+                // cout << "this is SharedPtr(make_s)"<<endl;
+            }
+        }
+        
+        //copy-ctor
+        SharedPtr(const SharedPtr& sp) : _p(sp._p), _refCount(sp._refCount){
+            if(_refCount){
+                ++(*_refCount);
+                // cout << "this is copy ctor"<<endl;
+            }
+        }
+
+
+        ~SharedPtr(){ release(); }
+
+        SharedPtr& operator=(const SharedPtr& sp){
+            if(this !=&sp){
+                release();
+
+                _p = sp._p;
+                _refCount = sp._refCount;
+                if(_refCount){
+                    ++(*_refCount);
+                    // cout << "this is op ="<<endl;
+                }
+            }
+            return *this;
+        }
+        size_t use_count() const{
+            // cout << "*_refCnt = "<<*_refCount <<endl;
+            return _refCount ? *_refCount : 0;
+        }
+
+        T* get(){
+            return _p;
+        }
+
+        T operator*() const {
+            return *_p;
+        }
+        T* operator->() const{
+            return _p;
+        }
+
+        void reset(){
+            release();
+            _p = nullptr;
+            _refCount = nullptr;
+        }
+        template<typename U>
+        void reset(U* ptr){
+            release();
+            _p = ptr;
+            _refCount = new size_t(1);
+        }
+        explicit operator bool() const{
+            return _p != nullptr;
+        }
+
+    private:
+        void release(){
+            if(_refCount && --(*_refCount)==0){
+                delete _p;
+                delete _refCount;
+                _p = nullptr;
+                _refCount = nullptr;
+            }
+        }
+
+        T* _p;
+        size_t* _refCount;
+        // std::atomic<size_t>* _refCount;
+
+
+
+
+
+
+};
+template<typename T, typename... Args>
+SharedPtr<T> make_shared(Args&&... args){
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
+
 
 
 #endif //SHARED_PTR
--- AP1400-2-HW4/src/main.cpp
+++ AP1400-2-HW4/src/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
     if (false) // make false to run unit-tests
     {
         // debug section
+        
         // UniquePtr<int> ptr{new int{10}};
         // // cout << *ptr <<endl;
         // auto up = make_unique<int>(42);
@@ -27,6 +28,42 @@ int main(int argc, char **argv)
         // delete tmp; // manual deletion
 
         // UniquePtr<int> ptr1{make_unique<int>(10)};
+
+        // SharedPtr<int> ptr{new int{10}};
+        // auto sp = make_shared<int>(10);
+        // int a{10};
+        // SharedPtr<int> ptr1 = ptr;
+        // SharedPtr<int> ptr2 = ptr1;
+
+        // SharedPtr<int> ptr{make_shared<int>(10)};
+        
+        // SharedPtr<int> ptr1{new int{10}};
+        // SharedPtr<int> ptr2{ptr1};
+
+        // SharedPtr<int> ptr1{new int{10}};
+        // SharedPtr<int> ptr2{new int{11}};
+        // ptr2 = ptr1;
+
+        // auto sp = make_shared<int>(10);
+        // std::cout << sp.use_count() <<endl;
+        // SharedPtr<int> ptr{new int{10}};
+        // std::cout << ptr.use_count() << std::endl;
+        // SharedPtr<int> ptr1{make_shared<int>(10)};
+        // std::cout << ptr1.use_count() << std::endl; // output: 1
+        // SharedPtr<int> ptr2{ptr1};
+        // std::cout << ptr1.use_count() << std::endl; // output: 2
+        // std::cout << ptr2.use_count() << std::endl; // output: 2
+        
+        // SharedPtr<int> ptr{new int{10}};
+        // std::cout << ptr.get() << std::endl; // output: raw pointer of the class
+        // std::cout << *ptr << std::endl; // output: 10
+        
+        // SharedPtr<std::string> ptr{new std::string{"hello"}};
+        // std::cout << ptr->length() << std::endl; // output: 5
+
+        // SharedPtr<std::string> ptr{new std::string{"hello"}};
+        // ptr.reset(new std::string{"nice"});
+        // std::cout << *ptr << std::endl; // output: nice
     }
     else
     {
