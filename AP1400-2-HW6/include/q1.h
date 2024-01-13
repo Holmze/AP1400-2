f13baccc56d13465e3001298d858889cbaa4432c
holmze <704062177@qq.com>
2024-01-13 16:34:51 +0800

    finish q1

--- AP1400-2-HW6/include/q1.h
+++ AP1400-2-HW6/include/q1.h
@@ -1,4 +1,28 @@
 #ifndef Q1_H
 #define Q1_H
+#include <iostream>
+#include <cmath>
+#include <functional>
+using std::cout;
+using std::endl;
+
+namespace q1{
+    template<typename T,typename Func>
+    T gradient_descent(T initial_value, T step_size, Func math_function){
+        T current_value = initial_value;
+        double df { (math_function(current_value + 0.00001) - math_function(current_value)) / 0.00001 };
+        
+        while(std::abs(df)>1e-6){
+            // cout << "Func("<<current_value<<")="<<math_function(current_value) <<endl;
+            current_value -= step_size*df;
+            df = (math_function(current_value + 0.00001) - math_function(current_value)) / 0.00001;
+        }
+        return current_value;
+    }
+    template<typename T,typename Func>
+    T gradient_descent(T initial_value, T step_size){
+        return gradient_descent(initial_value, step_size, Func());
+    }
+};
 
 #endif //Q1_H
--- AP1400-2-HW6/src/main.cpp
+++ AP1400-2-HW6/src/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <cmath>
 
 #include "q1.h"
 #include "q2.h"
 #include "q3.h"
 #include "q4.h"
+using std::cout;
+using std::endl;
 
 int main(int argc, char **argv)
 {
     if (false) // make false to run unit-tests
     {
         // debug section
-
+        auto min1 = q1::gradient_descent(0.01, 0.1, cos);
+        cout << min1<<endl;
+        min1 = q1::gradient_descent(0.01, 0.01, cos);
+        cout << min1<<endl;
     }
     else
     {
